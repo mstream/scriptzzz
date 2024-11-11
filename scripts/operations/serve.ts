507e49d792103e38da9b238419c6f3716de78d44
mstream <maciej.laciak@gmail.com>
2024-11-11 08:32:21 +0000

    fix: make the "serve" script wait until user stops it

--- scripts/operations/serve.ts
+++ scripts/operations/serve.ts
@@ -9,6 +9,9 @@ type Result = {
 
 export default async function(): Promise<Result> {
   const context = await esbuild.context(esbuildCommonBuildOptions)
-  const { host, port } = await context.serve({ servedir: 'dist' })
+  const { host, port } = await context.serve({
+    host: "127.0.0.1",
+    servedir: "dist",
+  })
   return { host, port, stop: context.dispose }
 }
--- scripts/serve.ts
+++ scripts/serve.ts
@@ -1,11 +1,19 @@
 #! /usr/bin/env -S nix develop --command deno run --allow-env --allow-net --allow-read --allow-run --allow-write
 
+import * as async from "@std/async"
 import { runScript } from "./common.ts"
 import build from "./operations/build.ts"
 import serve from "./operations/serve.ts"
 
 runScript(async () => {
   await build()
-  const { host, port } = await serve()
+  const { host, port, stop } = await serve()
   console.info(`Development server runs at http://${host}:${port}`)
+  console.info("Ctrl+C to stop")
+  let scheduledToStop = false
+  Deno.addSignalListener("SIGINT", () => scheduledToStop = true)
+  while (!scheduledToStop) {
+    await async.delay(500)
+  }
+  await stop()
 }) 
